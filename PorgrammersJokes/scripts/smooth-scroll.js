515aed2a8858bd8a7b2d387922da995681b9b71f
Yoan Petrushinov <i_onk_o@abv.bg>
2017-03-21 10:35:18 +0200

    Scrolling, heading
    Added smooth scrolling (JS) + Heading on last section

---
+++ PorgrammersJokes/scripts/smooth-scroll.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.social-block').on('mouseenter', 'img', function(event) {
+        event.preventDefault();
+        $(this).closest('.social-block').addClass('animated rotateIn');
+    });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
